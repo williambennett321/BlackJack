9de89a4bbe36a063a4536fc9a4ed038218d0c539
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-10-17 12:15:55 -0400

    accumulator for poker chips functional

--- js/app.js
+++ js/app.js
@@ -18,9 +18,7 @@ let valueOfCard = null
 let addRandomCard
 
 /*------------------------ Cached Element References ------------------------*/
-let add25 = document.querySelector("#25")
-let add50 = document.querySelector("#50")
-let add100 = document.querySelector("#100")
+let betOptions = document.querySelector (".bet-options")
 
 let playerHand = document.querySelector(".player-hand")
 let dealerHand = document.querySelector(".dealer-hand")
@@ -32,6 +30,22 @@ let placeBet = document.querySelector(".place-bet")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
+betOptions.addEventListener("click", (evt) => {
+  
+  if (evt.target.id === "25") {
+    playerBet += betOption1
+  } else if (evt.target.id === "50") {
+    playerBet += betOption2
+  } else if (evt.target.id === "100") {
+    playerBet += betOption3
+  }
+  
+  bet.innerHTML = `Current Bet: $${playerBet}`
+
+})
+  
+
+
 
 
 
@@ -42,3 +56,5 @@ init()
 function init() {
 deckOfCards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 }
+
+
