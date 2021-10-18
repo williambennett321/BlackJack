63e0bd539db51e444d5a6aafe3e8a5c748d0d23b
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-10-18 16:34:09 -0400

    add deployment branch

--- js/app.js
+++ js/app.js
@@ -12,8 +12,8 @@ const betOption3 = 100
 let deckOfCards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
 let playerBet = null
-let valueOfPHand = null
-let valueOfDHand = null
+let PHand = null
+let DHand = null
 let card = ""
 let valueOfCard = null
 let addRandomCard = ""
@@ -94,9 +94,13 @@ if (pocketValue < playerBet) {
 }
 
 // A function that will randomize the cards given
-let newCard = () => {
+let newRandomCard = () => {
   let cardString = deckOfCards[Math.floor(Math.random() * deckOfCards.length)]
   return cardString
+}
+let addCardToHand = () => {
+  let newCard = document.createElement("div")
+  
 }
 
 let valueOfCards = (cardString) => {
@@ -127,3 +131,5 @@ let valueOfCards = (cardString) => {
   }
   console.log(valueOfCard)
 }
+
+
