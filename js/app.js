164ee6b1296c567543cdb57a87b3df857e625cbd
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-10-19 22:12:28 -0400

    refactoring code

--- js/app.js
+++ js/app.js
@@ -55,8 +55,19 @@ betOptions.addEventListener("click", (evt) => {
 })
 // by pressing place bet, I want the game to start
 placeBet.addEventListener("click", () => {
+  if (pocketValue < playerBet) {
+
+    pocket.innerHTML = "Invalid Bet: You don't have enough money"
+  } else if (pocketValue >= playerBet) {
+      pocketValue -= playerBet
+      pocket.innerHTML = `Balance: $${pocketValue}`
+      init()
+      // if the bet is accepted, cards will be distributed to both players
+    } 
+    
   
-  init()
+  
+
 })
 
 // setting a function to hit and stand
@@ -75,22 +86,14 @@ if (evt.target.id === "hit") {
 
 /*-------------------------------- Functions --------------------------------*/
 
-init()
+
 // I want the deck to reset when I run the function
 function init() {
 deckOfCards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 // run through conditionals to prevent bet from being over pocket value
 
-if (pocketValue < playerBet) {
-
-  pocket.innerHTML = "Invalid Bet: You don't have enough money"
-} else if (pocketValue >= playerBet) {
-    pocketValue -= playerBet
-    pocket.innerHTML = `Balance: $${pocketValue}`
-    // if the bet is accepted, cards will be distributed to both players
-    
-  } 
-  distributeCards()
+distributeCards()
+  
 }
 
 // A function that will randomize the cards given
