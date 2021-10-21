b464eb5d6aa2e0434328e439ebdba3383b1a99c7
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-10-21 12:21:37 -0400

    add to deployment

--- js/app.js
+++ js/app.js
@@ -8,15 +8,8 @@
 /*---------------------------- Variables (state) ----------------------------*/
 let deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
-<<<<<<< HEAD
-let playerBet = null
-let PHand = null
-let DHand = null
-let card = ""
-=======
 let playerTotal = 0
 let dealerTotal = 0
->>>>>>> main
 let valueOfCard = null
 let p1Hand = []
 let p2Hand = []
