14ea39bf384af5c16b18b9a6f58aef4e3217f41b
williambennett321 <william_bennett@DESKTOP-U4DJP4P.localdomain>
2021-10-21 11:22:08 -0400

    Tall minimal functionality complete

--- js/app.js
+++ js/app.js
@@ -8,9 +8,8 @@
 /*---------------------------- Variables (state) ----------------------------*/
 let deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
 
-let numPHand = null
-let numDHand = null
-let card = ""
+let playerTotal = 0
+let dealerTotal = 0
 let valueOfCard = null
 let p1Hand = []
 let p2Hand = []
@@ -29,7 +28,7 @@ let winMsg = document.querySelector(".winLossMess")
 let startBtn = document.getElementById("start")
 let hitBtn = document.getElementById("hit")
 let standBtn = document.getElementById("stand")
-
+let resetBtn = document.querySelector(".reset-btn")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -41,7 +40,7 @@ startBtn.addEventListener("click", handleStart)
 hitBtn.addEventListener("click", hit)
 standBtn.addEventListener("click", stand)
 
-  
+resetBtn.addEventListener("click", init)
 
 
 
@@ -56,15 +55,21 @@ standBtn.addEventListener("click", stand)
 init()
 
 function init() {
+  let deck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
   p1Hand = []
   p2Hand = []
   gameDeck = shuffle(deck)
+  winMsg.innerText = ""
+  playerTotal = 0
+  dealerTotal = 0
+  valueOfCard = null
   render()
 }
 
 function handleStart() {
   // Deal two cards to each player
   let cardToDraw = gameDeck.pop()
+  console.log(cardToDraw)
   p1Hand.push(cardToDraw)
   cardToDraw = gameDeck.pop()
   p1Hand.push(cardToDraw)
@@ -75,7 +80,7 @@ function handleStart() {
   console.log(p1Hand, p2Hand)
   getHandVal(p1Hand)
   getHandVal(p2Hand)
-  compareHands()
+  getTotalForHand()
   render()
 
   
@@ -84,13 +89,15 @@ function handleStart() {
 function hit() {
   let cardToDraw = gameDeck.pop()
   p1Hand.push(cardToDraw)
-  getHandVal(p1Hand)
-  console.log(p1Hand)
+  playerTotal += valueOfCards(cardToDraw)
+  if (playerTotal > 21) {
+    winMsg.innerHTML = "You Bust, Dealer Wins"
+  }
   render()
 }
 
 function stand() {
-return p1Hand
+compareHands()
 }
 
 function shuffle(cardsToShuffle) {
@@ -133,40 +140,28 @@ function getHandVal(hand) {
   return total
 }
 
-function compareHands() {
-  let playerTotal = 0
-  let dealerTotal = 0
-  let playerValue = getHandVal(p1Hand)
+function getTotalForHand() {
+  playerValue = getHandVal(p1Hand)
   playerTotal += playerValue
   console.log(playerTotal)
   let dealerValue = getHandVal(p2Hand)
   dealerTotal += dealerValue
 console.log(dealerTotal)
-  winner(playerTotal, dealerTotal)
 
 }
 
-
-
-  // conditional that  renders loss if player or dealer bust
-  
-  // conditional that renders win if player or dealer have 21
-
-  //conditional that renders win if player > dealer vice versa
-
-
-function winner(valueOfPHand, valueOfDHand) {
-  if (valueOfPHand === 21 && valueOfDHand !== 21) {
+function compareHands() {
+  if (playerTotal === 21 && dealerTotal !== 21) {
     winMsg.innerHTML = "Congratulations Player is the winner"
-  } else if (valueOfDHand === 21 && valueOfPHand !== 21) {
+  } else if (dealerTotal === 21 && playerTotal !== 21) {
     winMsg.innerHTML = "Congratulations Dealer is the winner"
-  } else if (valueOfPHand === valueOfDHand) {
+  } else if (playerTotal === dealerTotal) {
     winMsg.innerHTML = "It's a draw"
-  } else if (valueOfPHand > valueOfDHand) {
+  } else if (playerTotal > dealerTotal) {
     winMsg.innerHTML = "Congratulations Player is the winner"
-  } else if (valueOfPHand < valueOfDHand) {
+  } else if (playerTotal < dealerTotal) {
     winMsg.innerHTML = "Congratulations Dealer is the winner"
-  } else if (valueOfPHand > 21) {
+  } else if (playerTotal > 21) {
     winMsg.innerHTML = "Player busted, dealer wins"
   } 
 }
@@ -174,10 +169,10 @@ function winner(valueOfPHand, valueOfDHand) {
 
 function valueOfCards(cardString) {
   if (cardString === "dA" || cardString === "hA" || cardString === "cA" || cardString === "sA") {
-    if (numPHand >= 10 || numDHand >= 10) {
+    if (playerTotal >= 10 || dealerTotal >= 10) {
       valueOfCard = 1
       return valueOfCard
-    } else {
+    } else if (10 >= playerTotal || dealerTotal <= 10) {
       valueOfCard = 11
       return valueOfCard
     }
